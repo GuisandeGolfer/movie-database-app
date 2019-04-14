ae2ae372a0b0b91b79180363eb63c201ca966dfa
Diego Guisande <dguisande873@gmail.com>
2019-04-14 03:59:30 +0000

    changed some spacing

--- app.js
+++ app.js
@@ -1,39 +1,21 @@
-var express = require("express");
-var app = express(); 
-var request = require("request"); 
+const   express = require("express"),
+        app = express(),
+        request = require("request"); 
 
 app.set("view engine","ejs");
 
-app.get("/", function(req,res){
-   res.render("search");  
-});
+app.get("/", (req,res) => res.render("search"));
 
-app.get("/results", function (req,res) {
+app.get("/results", (req,res) => {
     // body...  //will display the user's input results from the omdb API
-    var query = req.query.search;   //params gives route info //and query gives URL search info
-    var url = "http://omdbapi.com/?s=" + query + "&apikey=thewdb"; //combine search in URL
-    request(url, function(error,response,body){ //add back into request 'request'
+    let query = req.query.search;   //params gives route info //and query gives URL search info
+    let url = "http://omdbapi.com/?s=" + query + "&apikey=thewdb"; //combine search in URL
+    request(url, (error,response,body) => { //add back into request 'request'
         if(!error && response.statusCode == 200){
-            var data = JSON.parse(body); 
+            let data = JSON.parse(body); 
             res.render("results", {data: data}); 
         }
     });
-    
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Movie app is listening");
-})
+app.listen(process.env.PORT, process.env.IP, () => console.log("Movie app is listening"));
